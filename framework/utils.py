84313a2092160ac4ffe2b2629bea00c10d81c049
rafiarnouk <rafi.arnouk@gmail.com>
2025-10-30 18:23:42 -0700

    Implement updated splitting procedurel

--- framework/utils.py
+++ framework/utils.py
@@ -1,4 +1,5 @@
 import scanpy as sc
+import numpy as np
 import random
 import re
 
@@ -27,6 +28,53 @@ def assign_splits(adata, split, exclude_ctrl=False):
     adata.obs.loc[adata.obs["perturbation"].isin(perts_train), "split"] = "train"
     adata.obs.loc[adata.obs["perturbation"].isin(perts_val), "split"] = "val"
     adata.obs.loc[adata.obs["perturbation"].isin(perts_test), "split"] = "test"
+
+# not super robust, will fix if need arises
+def assign_splits_proportional(adata, split, seed=0):
+    random.seed(seed)
+    unique_perts_counts = adata.obs["perturbation"].value_counts().to_dict()
+    pert_counts = {pert: count for pert, count in unique_perts_counts.items() if not re.search(r"ctrl|control", pert, flags=re.IGNORECASE)}
+    ctrl_counts = {pert: count for pert, count in unique_perts_counts.items() if re.search(r"ctrl|control", pert, flags=re.IGNORECASE)}
+
+    # weighted random pick until amount of cells in test passes threshold
+    total_cells_count = sum(pert_counts.values())
+    test_target = split["test"] * total_cells_count
+    test_perts_count = 0
+    possible_test_perts = list(pert_counts.keys())
+    test_pert_weights = list(pert_counts.values())
+    perts_test = []
+    while test_perts_count < test_target:
+        chosen_pert = random.choices(possible_test_perts, test_pert_weights)[0]
+        perts_test.append(chosen_pert)
+        test_perts_count += pert_counts[chosen_pert]
+
+        # remove chosen pert from selection pool
+        index = possible_test_perts.index(chosen_pert)
+        possible_test_perts.pop(index)
+        test_pert_weights.pop(index)
+        print(f"chose {chosen_pert}, so now {test_perts_count=}")
+    
+    # assign test perts
+    adata.obs.loc[adata.obs["perturbation"].isin(perts_test), "split"] = "test"
+    
+    # assign controls to train
+    ctrls = list(ctrl_counts.keys())
+    adata.obs.loc[adata.obs["perturbation"].isin(ctrls), "split"] = "train"
+
+    # split remaining cells (not perts) to train/val
+    train_target = split["train"] * total_cells_count
+    val_target = split["val"] * total_cells_count
+    train_target -= sum(ctrl_counts.values())
+    prob_train = train_target / (train_target + val_target)
+
+    # mask for rows not in test
+    mask_rest = adata.obs["split"].isna()
+
+    # generate random numbers for remaining rows
+    rand_vals = np.random.rand(mask_rest.sum())
+
+    # assign train/val based on probability x
+    adata.obs.loc[mask_rest, "split"] = np.where(rand_vals < prob_train, "train", "val")
 
 # returns dictionary mapping perturbations found in adata object to genes being perturbed
 def get_perturbed_genes_map(adata, separator="_"):
