0c7e4a8c8eaaa28c791d95e1dc9e3ab4c96b84c4
rafiarnouk <rafi.arnouk@gmail.com>
2025-12-31 18:22:21 -0800

    Update baseline predictor script to take multiple files at once

--- framework/baseline_predictor.py
+++ framework/baseline_predictor.py
@@ -9,24 +9,29 @@ import numpy as np
 # example usage: 
 """
 python baseline_predictor.py \
-  --data_path ../data/preprocessed/adamson_preprocessed_0.h5ad \
-  --write_path ../data/predictions/adamson_pred_baseline_0.h5ad
+  --data_files norman_preprocessed_0 norman_preprocessed_1 norman_preprocessed_2 \
+  --write_filenames norman_pred_baseline_0 norman_pred_baseline_1 norman_pred_baseline_2 \
+  --data_path /scratch/st-jiaruid-1/rarnou01/preprocessed \
+  --write_path /scratch/st-jiaruid-1/rarnou01/results
 """
 
 def main():
     parser = argparse.ArgumentParser(description="Preprocess and clean dataset")
+    parser.add_argument("--data_files", nargs="*", type=str, help="Files containing adata objects to add baseline preds to")
+    parser.add_argument("--write_filenames", nargs="*", type=str, help="Names for output adata object files")
     parser.add_argument("--data_path", type=str, help="Path to adata object (object should have assigned splits)")
     parser.add_argument("--write_path", type=str, help="Path to write to")
     args = parser.parse_args()
+    data_files = args.data_files
+    write_filenames = args.write_filenames
     data_path = args.data_path
     write_path = args.write_path
 
-    adata = sc.read(data_path)
-    predict_mean(adata)
-
-    # NOTE if dealing with larger datasets might have to run on arc so update to export to scratch instead
-    print(f"Saving to {write_path}")
-    sc.write(write_path, adata)
+    for file, output_filename in zip(data_files, write_filenames):
+        adata = sc.read(f"{data_path}/{file}.h5ad")
+        predict_mean(adata)
+        print(f"Saving to {write_path}/{output_filename}.h5ad")
+        sc.write(f"{write_path}/{output_filename}.h5ad", adata)
 
 def predict_mean(adata):
     mask_not_test = adata.obs["split"].isin(["train", "val"])
@@ -40,6 +45,5 @@ def predict_mean(adata):
     mask_test = adata.obs["split"] == "test"
     adata.layers["pred_baseline_mean"][mask_test] = mean_gene_expression
     
-
 if __name__ == "__main__":
     main()
